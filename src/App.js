0be1440c403e1aa3496422771e25fd3e03a40fb7
deboragaleano <dgaleanoservian@gmail.com>
2020-08-05 17:34:17 +0200

    add campslist

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import CampsList from './campgrounds/CampsList';
 
 function App() {
   return (
     <div className="App">
       <h1>Camp norge App</h1>
+      <CampsList /> 
     </div>
   );
 }
---
+++ src/campgrounds/CampsList.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function CampsList() {
+    return <h1>CampsList</h1>
+}
+
