7676490444f972ea2874f666166adeed4af4a279
deboragaleano <dgaleanoservian@gmail.com>
2020-08-07 12:57:09 +0200

    add camp/new

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom'; 
 import CampsList from './components/CampsList';
 import Users from './components/Users';
+import NewCamp from './components/NewCamp';
 import Navbar from './components/Navbar';
 import './App.css';
 
@@ -9,10 +10,11 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      
+
       <Switch>
         <Route exact path='/' component={CampsList} />
         <Route exact path='/users' component={Users} />
+        <Route exact path='/camps/new' component={NewCamp} />
         <Redirect to='/' />
       </Switch>
     </div>
---
+++ src/components/NewCamp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NewCamp = () => {
+    return <h1>This is going to be a form</h1>
+}
+
+export default NewCamp; 
