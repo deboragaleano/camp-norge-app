c1ddd12a8fb31911a45c2dc42804c13393f8fc67
deboragaleano <dgaleanoservian@gmail.com>
2020-08-10 11:02:46 +0200

    fix navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,6 @@ import IconButton from '@material-ui/core/IconButton';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuIcon from '@material-ui/icons/Menu';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import {withStyles} from '@material-ui/core/styles';
 import styles from '../styles/NavbarStyles'; 
---
+++ src/components/UserCamps.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const UserCamps = (props) => {
+    return <h1>user camps</h1>
+}
+
+export default UserCamps;
+
